085b05a6a307eaf2a858e189167e62bb48b5f273
Kevin Muldoon <kmuldoon@mac.com>
2022-11-21 06:52:18 -0500

    Added Adobe Spectrum

--- src/app/constants/weightedTargets.ts
+++ src/app/constants/weightedTargets.ts
@@ -13,10 +13,11 @@ export const Options = [
     {value: '1', label: 'Genome', message: ''},
     {value: '2', label: 'IBM Carbon', message: ''},
     {value: '3', label: 'SalesForce Lightning', message: ''},
-    {value: '4', label: 'Ant', message: ''},
-    {value: '5', label: 'Material', message: ''},
-    {value: '6', label: 'Accessible Palette', message: ''},
-    {value: '7', label: 'ColorBox', message: ''},
+    {value: '4', label: 'Adobe Spectrum', message: ''},
+    {value: '5', label: 'Ant', message: ''},
+    {value: '6', label: 'Material', message: ''},
+    {value: '7', label: 'Accessible Palette', message: ''},
+    {value: '8', label: 'ColorBox', message: ''},
 ];
 
 enum WeightedTargetsOptions {
@@ -24,11 +25,40 @@ enum WeightedTargetsOptions {
     Genome,
     Carbon,
     Lightning,
+    AdobeSpectrum,
     Ant,
     Material,
     AccessiblePalette,
     ColorBox,
 }
+
+const weightedTargets_adobespectrum: weightedTargetsColumn = {
+    rows: [
+        {target: 0, weight: undefined},
+        {target: 5, weight: undefined},
+        {target: 10, weight: undefined},
+        {target: 15, weight: '1300'},
+        {target: 20, weight: undefined},
+        {target: 25, weight: '1200'},
+        {target: 30, weight: '1100'},
+        {target: 35, weight: '1000'},
+        {target: 40, weight: undefined},
+        {target: 45, weight: '900'},
+        {target: 50, weight: '800'},
+        {target: 55, weight: undefined},
+        {target: 60, weight: '700'},
+        {target: 65, weight: '600'},
+        {target: 70, weight: undefined},
+        {target: 75, weight: '500'},
+        {target: 80, weight: '400'},
+        {target: 85, weight: '300'},
+        {target: 90, weight: '200'},
+        {target: 95, weight: '100'},
+        {target: 97.5, weight: undefined},
+        {target: 100, weight: undefined},
+    ],
+    neutrals: [],
+};
 
 export const weightedTargets = (index: WeightedTargetsOptions): weightedTargetsColumn => {
     switch (index) {
@@ -38,6 +68,8 @@ export const weightedTargets = (index: WeightedTargetsOptions): weightedTargetsC
             return weightedTargets_carbon;
         case WeightedTargetsOptions.Lightning:
             return weightedTargets_lightning;
+        case WeightedTargetsOptions.AdobeSpectrum:
+            return weightedTargets_adobespectrum;
         case WeightedTargetsOptions.Ant:
             return weightedTargets_ant;
         case WeightedTargetsOptions.AccessiblePalette:
